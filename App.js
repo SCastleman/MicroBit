5fdf33b052668f077751cda4218939a34b2c21b6
Sam <samuel.castleman@ibm.com>
2020-11-13 16:37:42 +1100

    added basic connection logic and temperature retrieval

--- App.js
+++ App.js
@@ -1,10 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- */
+//@flow
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -12,17 +8,89 @@ import {
   View,
   Text,
   StatusBar,
+  Button,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Base64} from 'js-base64';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import {BleManager, Device} from 'react-native-ble-plx';
+
+const temperatureServiceUUID: string = 'e95d6100-251d-470a-a062-fa1922dfa9a8';
+const temperatureCharacteristicUUID: string =
+  'e95d9250-251d-470a-a062-fa1922dfa9a8';
 
 const App = () => {
+  const manager = new BleManager();
+  const [device, setDevice] = useState(new Device());
+  const [temperatureArray, setTemperatureArray] = useState<Array<Number>>([]);
+
+  const scanAndConnect = async () => {
+    // Attempt to retrieve a stored UUID from a previous session,
+    // and if successful, connect to that device and update the
+    // state with it.
+    const storedId = await AsyncStorage.getItem('microBitId');
+    if (storedId) {
+      console.log('found previous device');
+      setDevice(await manager.connectToDevice(storedId));
+      return;
+    }
+
+    // If there was no stored ID, start the device search and
+    // provide it with a callback that, on finding a device with
+    // a name containing "micro:bit", stops scanning, updates the
+    // state and saves the ID for future reference.
+    manager.startDeviceScan(null, null, async (error, tempDevice) => {
+      if (error) {
+        console.log('error: ', error);
+        return;
+      }
+      console.log('device: ', tempDevice.name);
+      if (tempDevice?.name?.includes('micro:bit')) {
+        manager.stopDeviceScan();
+        AsyncStorage.setItem('microBitID', tempDevice.id);
+        setDevice(tempDevice);
+      }
+    });
+  };
+
+  const postConnect = async () => {
+    await device.discoverAllServicesAndCharacteristics();
+    const chars = await device.readCharacteristicForService(
+      temperatureServiceUUID,
+      temperatureCharacteristicUUID,
+    );
+    const val = Base64.toUint8Array(chars.value);
+    chars.monitor((error, char) => {
+      if (error) {
+        return;
+      }
+    });
+    console.log('temperature', Base64.toUint8Array(chars.value)[0]);
+  };
+
+  useEffect(() => {
+    const subscription = manager.onStateChange((state) => {
+      console.log(state);
+      if (state === 'PoweredOn') {
+        scanAndConnect();
+        subscription.remove();
+      }
+    }, true);
+  }, []);
+
+  useEffect(() => {
+    if (device.id) {
+      device
+        .connect()
+        .then(() => postConnect())
+        .catch((e) => console.log(e));
+    }
+  }, [device]);
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
@@ -30,40 +98,7 @@ const App = () => {
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
+          <Button title="Other" onPress={() => postConnect()} />
         </ScrollView>
       </SafeAreaView>
     </>
--- android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
+++ android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -11,6 +11,8 @@ import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.ArrayList;
 
+// @react-native-async-storage/async-storage
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 // react-native-ble-plx
 import com.polidea.reactnativeble.BlePackage;
 
@@ -58,6 +60,7 @@ public class PackageList {
   public ArrayList<ReactPackage> getPackages() {
     return new ArrayList<>(Arrays.<ReactPackage>asList(
       new MainReactPackage(mConfig),
+      new AsyncStoragePackage(),
       new BlePackage()
     ));
   }
